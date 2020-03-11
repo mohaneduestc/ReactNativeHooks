f51631a9d0389639bc97b117676791abbb613517
Mohaned Giess <mohaneduestc@gmail.com>
2020-03-11 17:15:14 +0200

    using-outside-api

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,17 +1,36 @@
 import React, { useState } from 'react'
 import { Text, View, StyleSheet } from 'react-native'
 import SearchBar from './SearchBar'
+import yelp from '../api/yelp'
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('')
+  const [results, setResults] = useState([])
+
+  const searchApi = async (searchTerm) => {
+      try{
+          const response = await yelp.get('/search',{
+              params:{
+                  limit:50,
+                  term: searchTerm,
+                  location:'san jose'
+              }
+          })
+          // console.log(response)
+          setResults(response.data.businesses)
+
+      }catch(err){
+          console.log(err)
+      }
+  }
   return (
     <View>
       <SearchBar
         term={term}
         onTermChange={text => setTerm(text)}
-        onTermSubmitted={() => console.log('Term was submitted')}
+        onTermSubmitted={()=>searchApi(term)}
       />
-      <Text>{term}</Text>
+      <Text>We have found {results.length}</Text>
     </View>
   )
 }
