cfaddd189331275ca121b78b1f96149b88766038
Mohaned Giess <mohaneduestc@gmail.com>
2020-04-10 20:13:25 +0200

    Advanced-State-Management-with-Context

--- App.js
+++ App.js
@@ -5,17 +5,36 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { enableScreens } from 'react-native-screens'
 import { ScreenOrientation } from 'expo'
-enableScreens()
-const Stack = createStackNavigator();
+import IndexScreen from './src/theScreen/IndexScreen'
+import { Provider } from './src/context/BlogContext'
+import ShowScreen from './src/theScreen/ShowScreen'
+import CreateScreen from './src/theScreen/CreateScreen'
+import EditScreen from './src/theScreen/EditScreen'
 
+import { Feather } from '@expo/vector-icons'
+
+enableScreens()
+
+const Stack = createStackNavigator()
 
 function MyStack() {
   return (
     <Stack.Navigator
-      initialRouteName="Search"
+      initialRouteName="Index"
       screenOptions={{
         gestureEnabled: true
       }}>
+      <Stack.Screen
+        name="Index"
+        component={IndexScreen}
+        options={{
+          headerTitle: "Indexs",
+        }}
+      />
+      <Stack.Screen name="Show" component={ShowScreen} />
+      <Stack.Screen name="Create" component={CreateScreen} />
+      <Stack.Screen name="Edit" component={EditScreen} />
+
       <Stack.Screen
         name="Search"
         component={SearchScreen}
@@ -29,11 +48,17 @@ function MyStack() {
   )
 }
 
-export default function App() {
+function App1() {
   return (
     <NavigationContainer>
       <MyStack />
     </NavigationContainer>
   )
 }
-
+export default () => {
+  return (
+    <Provider>
+      <App1 />
+    </Provider>
+  )
+}
---
+++ src/context/BlogContext.js
@@ -0,0 +1,48 @@
+import createDataContacts from './createDataContacts'
+
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case 'edit_blogpost':
+      return state.map((blogPost) => {
+        return blogPost.id === action.payload.id ? action.payload : blogPost
+      })
+    case 'add_blogpost':
+      return [
+        ...state,
+        {
+          id: Math.floor(Math.random() * 99999),
+          title: action.payload.title,
+          content: action.payload.content,
+        },
+      ]
+    case 'delete_blogpost':
+      return state.filter((blogPost) => blogPost.id !== action.payload)
+    default:
+      return state
+  }
+}
+const addBlogPost = (dispatch) => {
+  return (title, content, callback) => {
+    dispatch({ type: 'add_blogpost', payload: { title, content } })
+    callback()
+  }
+}
+
+const editBlogPost = (dispatch) => {
+  return (id, title, content, callback) => {
+    dispatch({ type: 'edit_blogpost', payload: { id, title, content } })
+    callback()
+  }
+}
+
+const deleteBlogPost = (dispatch) => {
+  return (id) => {
+    dispatch({ type: 'delete_blogpost', payload: id })
+  }
+}
+
+export const { Context, Provider } = createDataContacts(
+  blogReducer,
+  { addBlogPost, deleteBlogPost, editBlogPost },
+  [{ title: 'TEST POST', content: 'TEST CONTENT', id: 1 }]
+)
